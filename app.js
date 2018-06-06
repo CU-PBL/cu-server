0548327fc13df161c009bad45d14dd7f6520c1fe
LikeJust <diddydwns135@gmail.com>
2018-06-06 15:31:53 +0900

    ccccc

---
+++ app.js
@@ -0,0 +1,46 @@
+/**
+ * Created by LikeJust on 2018-06-06.
+ */
+const express = require('express');
+const app = express();
+const admin = require('firebase-admin');
+
+var serviceAccount = require('./key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+
+
+app.get('/product/list', (req, res) => {
+    db.collection('cu-pbl/').get().then(qs => {
+
+        const productArr = [];
+
+        qs.forEach(x => {
+            productArr.push(x.data())
+        });
+
+        res.send(
+            productArr
+        );
+    })
+});
+
+app.post('/', (req, res) => {
+
+});
+
+
+app.listen(3000, () => {
+    console.log('server');
+    var docRef = db.collection('users').doc('alovelace');
+
+    var setAda = docRef.set({
+        first: 'Ada',
+        last: 'Lovelace',
+        born: 1815
+    });
+});
---
+++ db_input.js
@@ -0,0 +1,40 @@
+/**
+ * Created by LikeJust on 2018-06-06.
+ */
+const admin = require('firebase-admin');
+
+const serviceAccount = require('./key.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+var docRef = db.collection('cu-pbl/');
+
+
+var fs = require('fs');
+var CsvReadableStream = require('csv-reader');
+
+var inputStream = fs.createReadStream('product_data.csv', 'utf8');
+
+inputStream
+    .pipe(CsvReadableStream({parseNumbers: true, parseBooleans: true, trim: true}))
+    .on('data', (row) => {
+
+        const data_obj = {
+            'category': row[0],
+            'sub_category': row[1],
+            'id': row[2],
+            'name': row[3],
+            'price': row[4]
+        };
+
+        docRef.add(data_obj);
+
+        console.log(row);
+    })
+    .on('end', () => {
+        console.log('No more rows!');
+    });
+
+
