d92af6230e62c4db8dd8ec6e6f29bd20193136f7
LikeJust <diddydwns135@gmail.com>
2018-06-06 19:58:43 +0900

    stock 데이터 넣는 방법도 추가

--- db_input.js
+++ db_input.js
@@ -9,32 +9,52 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-var docRef = db.collection('cu-pbl/');
+// var docRef = db.collection('cu-pbl/');
+var docRef = db.collection('cu-stock/');
 
 
 var fs = require('fs');
 var CsvReadableStream = require('csv-reader');
 
-var inputStream = fs.createReadStream('product_data.csv', 'utf8');
+var inputStream = fs.createReadStream('StockData.csv', 'utf8');
 
 inputStream
     .pipe(CsvReadableStream({parseNumbers: true, parseBooleans: true, trim: true}))
     .on('data', (row) => {
 
-        const data_obj = {
-            'category': row[0],
-            'sub_category': row[1],
-            'id': row[2],
-            'name': row[3],
-            'price': row[4]
+        const dataObj = {
+            id: row[0],
+            stock: row[1]
         };
 
-        docRef.add(data_obj);
-
-        console.log(row);
+        docRef.add(dataObj);
+        console.log(dataObj);
     })
     .on('end', () => {
         console.log('No more rows!');
     });
 
+/*
+ var inputStream = fs.createReadStream('product_data.csv', 'utf8');
 
+ inputStream
+ .pipe(CsvReadableStream({parseNumbers: true, parseBooleans: true, trim: true}))
+ .on('data', (row) => {
+
+ const data_obj = {
+ 'category': row[0],
+ 'sub_category': row[1],
+ 'id': row[2],
+ 'name': row[3],
+ 'price': row[4]
+ };
+
+ docRef.add(data_obj);
+
+ console.log(row);
+ })
+ .on('end', () => {
+ console.log('No more rows!');
+ });
+
+ */
